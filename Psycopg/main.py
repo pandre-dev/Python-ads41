7681624811985325d7b0417cfd09dcea997df4c7
Paulo André <paulo92lima@gmail.com>
2019-10-10 20:21:10 -0300

    "Adicionado menu com 5 opções no crud PostgreSQL"

--- Psycopg/main.py
+++ Psycopg/main.py
@@ -2,9 +2,39 @@ from operacoes_bd import Operacoes
 
 if __name__ == "__main__":
     operacoes = Operacoes()
-    autor_livro = str(input("Digite o autor do livro: "))
-    assunto_livro = int(input("Digite o ID referente ao assunto: "))
-    editora_livro = int(input("Digite o ID referente à editora: "))
+    continuando = True
 
-    operacoes.salvar_livros(autor_livro, assunto_livro, editora_livro)
-    operacoes.buscar_livros()
+    print(f"\nCadastro de livros")
+    while continuando:
+        print(f"\n1 - Registrar livro"
+              f"\n2 - Mostrar todos"
+              f"\n3 - Buscar livro pelo ID"
+              f"\n4 - Editar livro"
+              f"\n5 - Excluir livro pelo ID"
+              f"\n\n0 - Sair")
+        op = int(input("\nDigite a opção desejada: "))
+
+        if op == 1:
+            autor = input("Digite o autor do livro: ")
+            assunto = int(input("Digite o ID do assunto: "))
+            editora = int(input("Digite o ID da editora: "))
+            operacoes.salvar_livros(autor, assunto, editora)
+        elif op == 2:
+            operacoes.mostrar_todos()
+        elif op == 3:
+            livro_id = int(input("Digite o ID do livro para a pesquisa: "))
+            operacoes.buscar_livro(livro_id)
+        elif op == 4:
+            livro_id = int(input("Digite o ID do livro a ser editado: "))
+            operacoes.buscar_livro(livro_id)
+            autor = input("Digite o novo autor do livro: ")
+            assunto = int(input("Digite o novo ID do assunto: "))
+            editora = int(input("Digite o novo ID da editora: "))
+            operacoes.editar_livros(autor, assunto, editora, livro_id)
+        elif op == 5:
+            livro_id = int(input("Digite o ID do livro a ser excluído: "))
+            operacoes.excluir_livro(livro_id)
+        elif op == 0:
+            continuando = False
+        else:
+            print("\nOpção indesejada. Tente novamente")
--- Psycopg/operacoes_bd.py
+++ Psycopg/operacoes_bd.py
@@ -7,8 +7,8 @@ class Operacoes():
         try:
             connection = Connection().get_connection()
             cursor = connection.cursor()
-            insert = f"""insert into livros (autor, assunto_id, editora_id) values ('{autor}', {assunto}, {editora});"""
-            cursor.execute(insert)
+            query_insert = f"""insert into livros (autor, assunto_id, editora_id) values ('{autor}', {assunto}, {editora});"""
+            cursor.execute(query_insert)
             connection.commit()
         except (Exception, psycopg2.DatabaseError) as er:
             print('Error: ', er)
@@ -17,12 +17,12 @@ class Operacoes():
                 cursor.close()
                 connection.close()
 
-    def buscar_livros(self):
+    def mostrar_todos(self):
         try:
             connection = Connection().get_connection()
             cursor = connection.cursor()
-            select = "select * from livros"
-            cursor.execute(select)
+            query_selectall = "select * from livros"
+            cursor.execute(query_selectall)
             livros = cursor.fetchall()
 
             print("\nTabela livros: ")
@@ -35,6 +35,58 @@ class Operacoes():
         except (Exception, psycopg2.DatabaseError) as er:
             print('Error: ', er)
 
+        finally:
+            if connection:
+                cursor.close()
+                connection.close()
+
+    def buscar_livro(self, livro_id):
+        try:
+            connection = Connection().get_connection()
+            cursor = connection.cursor()
+            query_select1 = f'select * from livros where id = {livro_id}'
+            cursor.execute(query_select1)
+            livro = cursor.fetchall()
+
+            for row in livro:
+                print("\nId = ", row[0])
+                print("autor = ", row[1])
+                print("assunto_id = ", row[2])
+                print("editora_id = ", row[3])
+
+        except (Exception, psycopg2.DatabaseError) as er:
+            print('Error: ', er)
+        finally:
+            if connection:
+                cursor.close()
+                connection.close()
+
+    def editar_livros(self, novo_autor, novo_assuntoid, nova_editoraid, livro_id):
+        try:
+            connection = Connection().get_connection()
+            cursor = connection.cursor()
+            query_update = f"""update livros set autor = '{novo_autor}', assunto_id = {novo_assuntoid}, editora_id = {nova_editoraid}
+                    where id = {livro_id}"""
+            cursor.execute(query_update)
+            connection.commit()
+            print('Registro alterado com sucesso!\n')
+        except (Exception, psycopg2.DatabaseError) as er:
+            print('Error: ', er)
+        finally:
+            if connection:
+                cursor.close()
+                connection.close()
+
+    def excluir_livro(self, livro_id):
+        try:
+            connection = Connection().get_connection()
+            cursor = connection.cursor()
+            query_delete = f"""delete from livros where id = {livro_id}"""
+            cursor.execute(query_delete)
+            connection.commit()
+            print('Registro excluído com sucesso!\n')
+        except (Exception, psycopg2.DatabaseError) as er:
+            print('Error: ', er)
         finally:
             if connection:
                 cursor.close()
